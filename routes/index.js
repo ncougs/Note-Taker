2670e0a4d488b933ede0e870a337e657b52ff121
ncougs <ncougan@gmail.com>
2021-08-04 16:54:32 +1000

    Adding comments

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const express = require('express');
 // Import our note routs
 const notes = require('./notes');
 
+// New instance of express
 const app = express();
 
+//route to use the notes requests. 
 app.use('/notes', notes);
 
 module.exports = app;
--- routes/notes.js
+++ routes/notes.js
@@ -1,7 +1,8 @@
+//files requried
 const { readFile, appendDataFile } = require('../helpers/readWrite');
 const notes = require('express').Router();
 
-const dbLocation = './db/db.json'
+const dbLocation = './db/db.json';
 
 
 // GET request for retrieving all notes
@@ -18,19 +19,24 @@ notes.post('/', (req, res) => {
     // If all requirements are there
     if (title && text) {
 
+        //define a new note
         const newNote = {
             "title": title,
             "text": text
         };
 
+        //append to datafile
         appendDataFile(newNote, dbLocation);
   
+        //send back response to API all
         res.send(`${title} has been saved`);
 
     }else {
+        //error in saving note
         res.send('Error in saving note.');
     }
 
 });
 
+//export notes route;
 module.exports = notes;
