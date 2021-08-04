a8619665cec9df65cc29bbdb7dcb354e9d004e06
ncougs <ncougan@gmail.com>
2021-08-04 15:32:37 +1000

    Adding read/write/append files

---
+++ helpers/readWrite.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+
+const writeFile = (location, data) => {
+    fs.writeFile(location, JSON.stringify(data, null, 4), (err) => {
+        if(err) {
+            console.error(err)
+        }
+        else {
+            console.info(`Data written to ${location}`)
+        }
+    });
+};
+
+const appendDataFile = (newData,location) => {
+    readFile(location)
+        .then(data => {
+            const jsonData = JSON.parse(data);
+            jsonData.push(newData);
+            writeFile(location, jsonData);
+            console.info(`Data appended to ${location}`);
+        })
+        .catch(error => console.error(error));
+};
+
+module.exports = {readFile, writeFile, appendDataFile};
+
+appendDataFile({
+    "title":"Test 2",
+    "text":"Test 2"
+},'./db/db.json');
