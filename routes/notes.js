7a47d5e6aa6d05128338dae889209ab521ed2067
ncougs <ncougan@gmail.com>
2021-08-04 16:51:40 +1000

    Adding new POST route to save a new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,12 +1,36 @@
 const { readFile, appendDataFile } = require('../helpers/readWrite');
 const notes = require('express').Router();
 
+const dbLocation = './db/db.json'
 
-// GET Route for retrieving all notes
+
+// GET request for retrieving all notes
 notes.get('/', (req, res) => {
-    readFile('./db/db.json').then(data => res.json(JSON.parse(data)))
+    readFile(dbLocation).then(data => res.json(JSON.parse(data)))
+});
+
+// POST request to add a new note
+notes.post('/', (req, res) => {
+ 
+    // getting title and text out of request body
+    const { title, text } = req.body;
+  
+    // If all requirements are there
+    if (title && text) {
+
+        const newNote = {
+            "title": title,
+            "text": text
+        };
+
+        appendDataFile(newNote, dbLocation);
+  
+        res.send(`${title} has been saved`);
+
+    }else {
+        res.send('Error in saving note.');
+    }
+
 });
 
 module.exports = notes;
-
-// readFile('./db/db.json').then(data => console.log(JSON.parse(data)));
